6d9033e1b0f39f9ec7d1aba718341d73e4d408b1
dan-ueno <ueno2005@gmail.com>
2021-04-26 11:38:11 -0300

    aula 14 exercícios realizados!

---
+++ semana4/funcoes/index.js
@@ -0,0 +1,110 @@
+// ----------------------> Exercicio 1 <---------------------------
+
+//a. em duas linhas: 10 e 50
+//b. nada apareceria no console, ele iria calcular e retornar o valor para lugar algum
+
+// ----------------------> Execicio 2 <---------------------------
+
+//a. Darvas Caio
+//b. Amanda Caio
+
+// ----------------------> Exercicio 3 <---------------------------
+
+//. verifica se o array possui números pares e salva um novo array com os valores pares ao quadrado. paresAoQuadrado xD
+
+// ----------------------> Exercicio 4 <---------------------------
+//a.
+function quemSouEu() {
+    console.log ("Eu sou o Daniel, tenho 30  anos, moro em São Paulo e sou estudante")
+}
+//b.
+// function quemEsTu(nome, idade, cidade, estudante) {
+//   let dadosConsolidados = [];
+//   dadosConsolidados[0] = nome;
+//   dadosConsolidados[1] = idade;
+//   dadosConsolidados[2] = cidade;
+//   if (estudante) {
+//     dadosConsolidados[3] = "sou";
+//   } else {
+//     dadosConsolidados[3] = "não sou";
+//   }
+//   console.log(
+//     `Eu sou ${dadosConsolidados[0]}, tenho ${dadosConsolidados[1]} anos, moro em ${dadosConsolidados[2]} e ${dadosConsolidados[3]} estudante`
+//   );
+//   return dadosConsolidados;
+// }
+
+
+// ----------------------> Exercicio 5 <---------------------------
+
+//a.
+// function somaDoisNumeros(numero1, numero2){
+// return numero1 + numero2
+// }
+// console.log(somaDoisNumeros(2,3))
+
+//b.
+// function primeiroEMaior(numero1, numero2){
+//     if (numero1 > numero2)
+//     return true
+//     else return false
+//     }
+
+//c.
+//hahahah nao escreva 10x xDD
+// function spam(string) {
+//     let texto = []
+//     for (let i=0; i<string.length; i++){
+//         texto+=string[i] + " "
+//     }
+//     for (i=0; i<10; i++){
+//         console.log (texto)
+//     }
+// }
+
+// ----------------------> Exercicio 6 <---------------------------
+
+//a.
+// function tamanhoArray(array){
+//     return array.lenght
+// }
+
+//b.
+// function numeroEPar(numero){
+//     if (numero%2 ===0)
+//     return true
+//     else
+//     return false
+// }
+
+//c. 
+
+// function quantidadePares(array) {
+//     let somaDePares = 0
+//     for (number of array) {
+//         if (number%2===0){
+//             somaDePares++
+//         }
+//     }
+//     return somaDePares
+//}
+
+//d.
+
+// function quantidadePares(array) {
+//     let somaDePares = 0
+//     for (number of array) {
+//         if (numeroEPar(number)){
+//             somaDePares++
+//         }
+//     }
+//     return somaDePares
+// }
+
+// ----------------------> Exercicio 7 <---------------------------
+
+
+// ----------------------> Desafio 1<---------------------------
+
+
+// ----------------------> Desafio 2<---------------------------
