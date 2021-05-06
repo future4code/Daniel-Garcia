2e432e5f2e829ba60801b0532883d6915f6d6ab3
dan-ueno <ueno2005@gmail.com>
2021-05-06 15:43:07 -0300

    aula21 realizada

--- semana5/insta4/src/App.js
+++ semana5/insta4/src/App.js
@@ -8,29 +8,94 @@ const MainContainer = styled.div`
   flex-direction: column;
   align-items: center;
 `
+const Inputs = styled.div`
+  border: 1px solid gray;
+  width: 300px;
+  margin-bottom: 10px;`
 
 class App extends React.Component {
-  render() {
-    return (
-      <MainContainer>
-        <Post
-          nomeUsuario={'Paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-                <Post
-          nomeUsuario={'Bruna'}
-          fotoUsuario={'https://picsum.photos/60/50'}
-          fotoPost={'https://picsum.photos/210/150'}
-        />
-                <Post
-          nomeUsuario={'Júlia'}
-          fotoUsuario={'https://picsum.photos/70/50'}
-          fotoPost={'https://picsum.photos/220/150'}
-        />
-      </MainContainer>
-    );
+  state = {
+    publicacoes: [
+      {
+        nome: "Paulinha",
+        fotoUser: "https://picsum.photos/50/50",
+        foto: "https://picsum.photos/200/150"
+      },
+      {
+        nome: "Bruna",
+        fotoUser: "https://picsum.photos/60/50",
+        foto: "https://picsum.photos/210/150"
+      },
+      {
+        nome: "Júlia",
+        fotoUser: "https://picsum.photos/70/50",
+        foto: "https://picsum.photos/220/150"
+      }
+    ],
+    valorInputUsuario: "",
+    valorInputFotoUser: "",
+    valorInputFotoPost: ""
   }
-}
 
+//COLOCAR OS HANDLES
+handleNomeUsuario = (event) => {
+  this.setState({ valorInputUsuario: event.target.value });
+};
+handleFotoUser = (event) => {
+  this.setState({ valorInputFotoUser: event.target.value });
+};
+handleFotoPost = (event) => {
+  this.setState({ valorInputFotoPost: event.target.value });
+};
+onClickPublicar = () => {
+  const novoPost = {
+    nome: this.state.valorInputUsuario,
+    fotoUser: this.state.valorInputFotoUser,
+    foto: this.state.valorInputFotoPost
+  };
+
+this.setState({
+  publicacoes: [...this.state.publicacoes, novoPost],
+  valorInputUsuario: "",
+  valorInputFotoUser: "",
+  valorInputFotoPost: ""
+})
+  };
+
+  render() {
+
+    const listaDePosts = this.state.publicacoes.map((postagens) =>{
+      return (
+        <Post
+          nomeUsuario = {postagens.nome}
+          fotoUsuario = {postagens.fotoUser}
+          fotoPost = {postagens.foto}
+        /> 
+    );
+});
+return (
+  <MainContainer>
+      {listaDePosts}
+        <Inputs>
+          <input
+            value={this.state.valorInputUsuario}
+            onChange={this.handleNomeUsuario}
+            placeholder={"Usuario"}
+          />
+          <input
+            value={this.state.valorInputFotoUser}
+            onChange={this.handleFotoUser}
+            placeholder={"link Foto"}
+          />
+          <input
+            value={this.state.valorInputFotoPost}
+            onChange={this.handleFotoPost}
+            placeholder={"Link foto do post"}
+          />
+          <button onClick={this.onClickPublicar}>Publicar</button>
+        </Inputs>
+  </MainContainer>
+);
+}
+}
 export default App;
