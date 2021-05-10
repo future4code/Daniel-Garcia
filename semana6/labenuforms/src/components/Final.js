6e045cbee84fc344467e5d282d2b33ebfbb54be2
dan-ueno <ueno2005@gmail.com>
2021-05-10 16:13:55 -0300

    entrega mvp aula 22

---
+++ semana6/labenuforms/src/components/Final.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {Container} from "./styled"
+
+class Final extends React.Component {
+  
+  render() {
+
+    return (
+      <Container>
+        <h1>O FORMULÁRIO ACABOU</h1>
+        <p>Muito obrigado por participar! Entraremos em contato!</p>
+      </Container>
+    );
+  }
+
+}
+
+export default Final;
